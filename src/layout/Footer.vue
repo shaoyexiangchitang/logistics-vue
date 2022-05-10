8886bfafaa7e296518607b3b03ac1e93711500c6
彭慧标 <2977369447@qq.com>
2022-05-10 10:12:46 +0800

    first commit

---
+++ src/layout/Footer.vue
@@ -0,0 +1,15 @@
+<template>
+  <a-layout-footer :style="{ textAlign: 'center', letterSpacing: '1px' }">
+    物流管理系统 ©2022 Created by xxx学院 - xxx
+  </a-layout-footer>
+</template>
+
+<script>
+export default {
+name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
