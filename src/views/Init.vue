a3b11b5f1bc596fddf9f02692b4376283bc7bedf
彭慧标 <2977369447@qq.com>
2022-05-26 17:01:33 +0800

    test  1 高元明->2 xxx

--- src/views/Init.vue
+++ src/views/Init.vue
@@ -24,7 +24,7 @@
       <a-button :loading="submitLoading" class="submit-btn" type="primary" @click="submitLogin">
         初始化系统
       </a-button>
-      <div class="des">物流管理系统 - Copyright 2021 高元明</div>
+      <div class="des">物流管理系统 - Copyright 2022 xxx</div>
     </div>
   </div>
 </template>
